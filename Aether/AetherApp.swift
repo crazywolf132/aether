5cab6c832eeee55d0aeb7eef55f72f0731fd1717
crazywolf132 <crazywolf132@gmail.com>
2025-02-11 10:20:27 +1100

    Initial Commit

---
+++ Aether/AetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AetherApp.swift
+//  Aether
+//
+//  Created by Brayden Moon on 11/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct AetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
